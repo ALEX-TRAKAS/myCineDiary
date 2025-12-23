58dbe871ebe8a140e697da1e3c10a036f08c29dc
Alex-Trakas <57215703+ALEX-TRAKAS@users.noreply.github.com>
2025-12-23 19:17:05 +0200

    Feat(ui):update web layout and search screen

--- app/web/_layout.tsx
+++ app/web/_layout.tsx
@@ -1,23 +1,26 @@
+import { WebFooter } from '@/src/components/webFooter';
 import { WebHeader } from '@/src/components/webHeader';
 import { Slot } from 'expo-router';
-import { useColorScheme } from 'react-native';
-import { Text, Theme, XStack, YStack } from 'tamagui';
+import { ScrollView, useColorScheme } from 'react-native';
+import { Theme, XStack, YStack } from 'tamagui';
 
 
 export default function WebLayout() {
   const colorScheme = useColorScheme();
-  return (
-      <Theme name={colorScheme === 'dark' ? 'dark' : 'light'}>
-        <YStack flex={1} background="$background">
-          {/* Top navbar */}
-          <XStack background="$primary" padding="$3" alignItems="center" justifyContent="space-between">
-            <Text fontWeight="700" color="$color">MyCineDiary</Text>
-             <WebHeader />
-          </XStack>
-          <YStack flex={1} padding="$4">
-          <Slot></Slot>{}
+    return (
+    <Theme name={colorScheme === 'dark' ? 'dark' : 'light'}>
+      <YStack flex={1} background="$background">
+        <XStack background="$primary" padding="$3" alignItems="center" justifyContent="space-between">
+          <WebHeader/>
+        </XStack>
+        <ScrollView  contentContainerStyle={{ flexGrow: 1 }}>
+          <YStack padding="$4" flex={1}>
+            <Slot/>
+            <WebFooter/>
           </YStack>
-        </YStack>
-      </Theme>
+        </ScrollView>
+      </YStack>
+    </Theme>
   );
 }
+
--- app/web/search.tsx
+++ app/web/search.tsx
@@ -1,9 +1,92 @@
-import { Text, YStack } from "tamagui";
+import { searchMediaAll } from "@/src/api/tmbdApi";
+import { TMDBMedia } from "@/types/tmdb";
+import { useCallback, useEffect, useState } from "react";
+import { View } from "react-native";
+import { Input, YStack } from "tamagui";
+import { MovieShowsList } from '../../src/components/movieShowsList';
+
+
+
+
 export default function Search() {
+   const [query, setQuery] = useState("");
+  const [data, setData] = useState<TMDBMedia[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+
+  const fetchSearch = useCallback(async () => {
+    if (loading || !query.trim() || !hasMore) return;
+
+    setLoading(true);
+
+    try {
+      const res = await searchMediaAll(query, page);
+      if (!res?.results || !Array.isArray(res.results)) {
+        throw new Error("Invalid response from server");
+      }
+
+      setData(prev => page === 1 ? res.results : [...prev, ...res.results]);
+
+
+      setHasMore(page < res.total_pages);
+      setPage(prev => prev + 1);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setLoading(false);
+    }
+  }, [query, page, loading, hasMore]);
+
+  useEffect(() => {
+    if (!query.trim()) return;
+
+    const timeout = setTimeout(() => {
+      setPage(1);
+      setData([]);
+      setHasMore(true);
+      fetchSearch();
+    }, 400);
+
+    return () => clearTimeout(timeout);
+  }, [query]);
+
+
+
   return (
-    <YStack f={1} jc="center" ai="center">
-      
-      <Text>TODO : Search movies & series</Text>
+    <YStack f={1} jc="center"  ai="center">
+      <YStack f={1} jc="center" >
+        <View>
+            <Input
+              height={40}
+              margin={12}
+              marginBottom={20}
+              width="800px"
+              paddingHorizontal={10}
+              paddingLeft={10}
+              paddingRight={10}
+              borderWidth={1}
+              borderColor="$primary"
+              fontSize={16}
+              textAlign="center"
+              color="white"
+              placeholder="Search movies & series"
+              placeholderTextColor="$primary"
+              value={query}
+              onChangeText={setQuery}
+              onSubmitEditing={() => {
+                setPage(1);
+                setData([]);
+                fetchSearch();
+              }}
+            />
+          </View>
+          <MovieShowsList
+            data={data}
+            loading={loading}
+            loadMore={fetchSearch}
+          />
+      </YStack>
     </YStack>
   );
 }
