3f28b65c69311da88e589360fb44fca50a96b7c3
Alex-Trakas <57215703+ALEX-TRAKAS@users.noreply.github.com>
2025-12-28 16:21:36 +0200

    Feat(ui):update movie/show details screen

--- app/details/[id].tsx
+++ app/details/[id].tsx
@@ -1,11 +1,14 @@
 import { WebHeader } from '@/src/components/webHeader';
+import { ArrowBigLeft } from '@tamagui/lucide-icons';
 import { Image } from 'expo-image';
 import { useLocalSearchParams } from 'expo-router';
 import { useEffect, useState } from 'react';
 import { Platform, ScrollView } from 'react-native';
-import { Text, YStack } from 'tamagui';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Button, Stack, Text, XStack, YStack } from 'tamagui';
 
 export default function Details() {
+  const insets = useSafeAreaInsets(); 
   const isWeb = Platform.OS === 'web';
   const { id, type } = useLocalSearchParams<{
     id: string;
@@ -43,6 +46,15 @@ export default function Details() {
   return (
     <ScrollView>
       <YStack f={1} gap="$4" p="$4" bg="$background">
+        <XStack px="$4" py="$3" pt={insets.top + 12} w="100%" ai="center" jc="space-between" bc="$surface">
+        <XStack ai="center">
+          <Stack width={40} height={40}>
+          </Stack>
+        <YStack position='absolute' left={0}>
+          <Button circular backgroundColor="$surface" icon={<ArrowBigLeft size={24} color={'#aaa'} />} />
+        </YStack>
+        </XStack>
+      </XStack>
         <Image
           source={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
           style={{ width: '100%', height: 400, borderRadius: 16 }}
