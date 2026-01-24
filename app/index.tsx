603afd84d391af5fabb57bcc37a5ae1e5b05508f
Alex-Trakas <57215703+ALEX-TRAKAS@users.noreply.github.com>
2026-01-24 23:52:07 +0200

    Refactor(WEBauth):changed back to platform check

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,9 @@
 import { Redirect } from "expo-router";
+import { Platform } from "react-native";
 
 export default function Index() {
+  if (Platform.OS === "web") {
+    return <Redirect href="/web/home" />;
+  }
   return <Redirect href="/home" />;
 }
