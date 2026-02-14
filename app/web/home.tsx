c09c2cf15f747ebb8d55d390e14faf50d8b085ce
Alex-Trakas <57215703+ALEX-TRAKAS@users.noreply.github.com>
2026-02-14 18:53:28 +0200

    Feat(UI)ADD tv trending carousel

--- app/web/home.tsx
+++ app/web/home.tsx
@@ -3,20 +3,22 @@ import { TrendingCarousel } from "@/src/components/trendingCarousel";
 import { TMDBMedia } from "@/types/tmdb";
 import { useEffect, useState } from "react";
 import { Separator, Text, YStack } from "tamagui";
-import { fetchTrending } from "../../src/api/tmbdApi";
+import { fetchTrendingMovies, fetchTrendingTv } from "../../src/api/tmbdApi";
 
 export default function Home() {
-  const [data, setData] = useState<TMDBMedia[]>([]);
-  const [loading, setLoading] = useState(false);
+  const [moviesData, setData] = useState<TMDBMedia[]>([]);
+  const [tvData, setTvData] = useState<TMDBMedia[]>([]);
+  const [loadingMovies, setLoadingMovies] = useState(false);
+  const [loadingTv, setLoadingTv] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const loadTrending = async () => {
-      setLoading(true);
+    const loadTrendingMovies = async () => {
+      setLoadingMovies(true);
       setError(null);
 
       try {
-        const res = await fetchTrending("movie", 1);
+        const res = await fetchTrendingMovies("movie", 1);
 
         if (!Array.isArray(res?.results)) {
           throw new Error("Invalid response");
@@ -26,11 +28,31 @@ export default function Home() {
         console.error(err);
         setError("Failed to load trending.");
       } finally {
-        setLoading(false);
+        setLoadingMovies(false);
       }
     };
 
-    loadTrending();
+    const loadTrendingTv = async () => {
+      setLoadingTv(true);
+      setError(null);
+
+      try {
+        const res = await fetchTrendingTv("tv", 1);
+
+        if (!Array.isArray(res?.results)) {
+          throw new Error("Invalid response");
+        }
+        setTvData(res.results.slice(0, 10));
+      } catch (err) {
+        console.error(err);
+        setError("Failed to load trending TV shows.");
+      } finally {
+        setLoadingTv(false);
+      }
+    };
+
+    loadTrendingMovies();
+    loadTrendingTv();
   }, []);
 
   return (
@@ -38,14 +60,24 @@ export default function Home() {
       <YStack px="$4" pt="$4" pb="$3" ai="center">
         <MediaSearch placeholder="Search movies, shows" inputWidth={600} />
         <Text fontSize="$8" fontWeight="700">
-          Trending
+          Trending Movies
         </Text>
         <Separator alignSelf="stretch" mx={16} borderColor="$gray8" />
       </YStack>
-
       <YStack px="$3" w="100%" alignSelf="center" maxWidth={900}>
         {error && <Text color="red">{error}</Text>}
-        <TrendingCarousel data={data} />
+        <TrendingCarousel data={moviesData} />
+      </YStack>
+      <Separator alignSelf="stretch" mx={16} borderColor="$gray8" />
+      <YStack px="$4" pt="$4" pb="$3" ai="center">
+        <Text fontSize="$8" fontWeight="700">
+          Trending TV Shows
+        </Text>
+        <Separator alignSelf="stretch" mx={16} borderColor="$gray8" />
+      </YStack>
+      <YStack px="$3" w="100%" alignSelf="center" maxWidth={900}>
+        {error && <Text color="red">{error}</Text>}
+        <TrendingCarousel data={tvData} />
       </YStack>
     </YStack>
   );
