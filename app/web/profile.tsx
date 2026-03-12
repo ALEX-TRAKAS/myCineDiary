ab99b3a360fef9736947fb4f487544278a5ce057
Alex-Trakas <57215703+ALEX-TRAKAS@users.noreply.github.com>
2026-03-12 23:59:39 +0200

    Feat(Profile)ADD profile screen

--- app/web/profile.tsx
+++ app/web/profile.tsx
@@ -1,19 +1,32 @@
 import { ProtectedRoute } from "@/src/components/auth/protectedRoute";
-import { Text, YStack } from "tamagui";
+import { LibraryList } from "@/src/components/libraryList";
+import { Paragraph, XStack, YStack } from "tamagui";
 
 export default function Profile() {
   return (
     <ProtectedRoute>
-      <YStack
-        flex={1}
-        justifyContent="center"
-        alignItems="center"
-        background="$background"
-      >
-        <Text fontSize="$8" fontWeight="700" color="$color">
-          Profile Page (Tabs)
-        </Text>
-      </YStack>
+      <XStack flex={1} backgroundColor="#111">
+        <YStack width={250} backgroundColor="#181818" padding="$5" gap="$3">
+          <Paragraph color="#fff" fontWeight="700" fontSize="$6" mb="$3">
+            Filters
+          </Paragraph>
+        </YStack>
+
+        <YStack flex={1}>
+          <Paragraph fontSize="$8" fontWeight="700" color="#fff" padding="$6">
+            Your Watchlist
+          </Paragraph>
+
+          <YStack flex={1}>
+            <LibraryList
+              data={[]}
+              loading={false}
+              loadMore={() => {}}
+              layout="vertical"
+            />
+          </YStack>
+        </YStack>
+      </XStack>
     </ProtectedRoute>
   );
 }
